36394a04a4c6105977ac01c57139c6911945cff5
Nishant Shrestha <155070221+nishant-sth@users.noreply.github.com>
2025-03-22 22:34:15 +0545

    refactor: migrate project structure from litle_lemon to elite_kitchen

--- elite_kitchen/wsgi.py
+++ elite_kitchen/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for litle_lemon project.
+WSGI config for elite_kitchen project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'litle_lemon.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'elite_kitchen.settings')
 
 application = get_wsgi_application()
