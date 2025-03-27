a293b14ca0844558aefc13abb907cddba0156ea2
Nishant Shrestha <155070221+nishant-sth@users.noreply.github.com>
2025-03-27 12:08:35 +0545

    refactor(settings.py)

--- elite_kitchen/settings.py
+++ elite_kitchen/settings.py
@@ -145,7 +145,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'shresthanis2255@gmail.com'
-EMAIL_HOST_PASSWORD = 'xzfs khuq fvlp gyam'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
 
